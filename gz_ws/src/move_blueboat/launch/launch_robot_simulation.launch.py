f895d16bb51bd1f0704c48b24da4e017d3387c57
Alberto Quattrini Li <alberto.quattrini.li@dartmouth.edu>
2026-08-09 11:42:16 -0400

    added second boat with topics mapped to the small boat

--- gz_ws/src/move_blueboat/launch/launch_robot_simulation.launch.py
+++ gz_ws/src/move_blueboat/launch/launch_robot_simulation.launch.py
@@ -24,6 +24,15 @@ def generate_launch_description():
                 '/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
                 # lidar
                 '/laser_scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
+                '/model/smallboat/joint/motor_port_joint/cmd_thrust@std_msgs/msg/Float64@ignition.msgs.Double',
+                '/model/smallboat/joint/motor_stbd_joint/cmd_thrust@std_msgs/msg/Float64@ignition.msgs.Double',
+                '/model/smallboat/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
+                '/smallboat/navsat@sensor_msgs/msg/NavSatFix@ignition.msgs.NavSat',
+                # camera
+                '/smallboat/camera@sensor_msgs/msg/Image@ignition.msgs.Image',
+                '/smallboat/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo',
+                # lidar
+                '/smallboat/laser_scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
             ],
             output='screen'
         ),
